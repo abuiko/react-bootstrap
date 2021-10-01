23e2bed887779ea3c89d856812e99e3f600102dd
Alena Buiko <abuiko.ma@gmail.com>
2021-09-30 20:18:11 -0400

    add layout components

--- src/App.js
+++ src/App.js
@@ -5,20 +5,25 @@ import { User } from "./User";
 import { Favorites } from "./Favorites";
 import { Basket } from "./Basket";
 import { NoMatch } from "./NoMatch";
+import { Layout } from "./components/Layout";
+
 
 
 function App() {
   return (
     <React.Fragment>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home}></Route>
-          <Route path="/user" component={User}></Route>
-          <Route exact path="/favorites" component={Favorites}></Route>
-          <Route exact path="/basket" component={Basket}></Route>
-          <Route component={NoMatch}></Route>
-        </Switch>
-      </Router>
+      <Layout>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home}></Route>
+            <Route path="/user" component={User}></Route>
+            <Route exact path="/favorites" component={Favorites}></Route>
+            <Route exact path="/basket" component={Basket}></Route>
+            <Route component={NoMatch}></Route>
+          </Switch>
+        </Router>
+      </Layout>
+
     </React.Fragment>
   );
 }
