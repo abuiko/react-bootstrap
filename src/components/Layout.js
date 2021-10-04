e4d3a3249ebabcc95605e6cd0f55e6cfce9372ae
Alena Buiko <abuiko.ma@gmail.com>
2021-10-04 15:02:36 -0400

    change layout frame settings

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,25 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import styled from "styled-components";
+
+const Styles = styled.div`
+    .container {
+        max-width: 1400px;
+        padding: 0;
+        margin: auto;
+    }
+
+`;
+
+
 
 export const Layout = (props) => {
     return (
-        <Container>
-            {props.children}
-        </Container>
+        <Styles>
+            <Container className="container">
+                {props.children}
+            </Container>
+        </Styles>
+
     )
 }
