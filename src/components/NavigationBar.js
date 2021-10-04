bc41c8bde03294385b4910874a8362f57152a42c
Alena Buiko <abuiko.ma@gmail.com>
2021-10-04 15:07:18 -0400

    move nav-links to the right

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -7,7 +7,9 @@ const Styles = styled.div`
     .navbar {
         background-color: #fff;
     }
-
+    .ml-auto {
+        margin-left: auto;
+    }
     .navbar-brand, .navbar-nav, .nav-link {
         color: #000;
 
@@ -15,20 +17,21 @@ const Styles = styled.div`
             color: #bbb;
         }
     }
+    
 `;
 
 
 export const NavigationBar = () => {
     return (
         <Styles>
-            <Navbar expand="lg" className="p-4">
+            <Navbar expand="lg" className="navbar p-4">
                 <Navbar.Brand href="/">Logo</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto">
-                        <Nav.Item><Nav.Link href="/user">User</Nav.Link></Nav.Item>
-                        <Nav.Item><Nav.Link href="/favorites">Favorites</Nav.Link></Nav.Item>
-                        <Nav.Item><Nav.Link href="/basket">Basket</Nav.Link></Nav.Item>
+                        <Nav.Item><Nav.Link className="nav-link" href="/user">User</Nav.Link></Nav.Item>
+                        <Nav.Item><Nav.Link className="nav-link" href="/favorites">Favorites</Nav.Link></Nav.Item>
+                        <Nav.Item><Nav.Link className="nav-link" href="/basket">Basket</Nav.Link></Nav.Item>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
