3c28df972980ffbf07f453a05c40155eee46301f
Alena Buiko <abuiko.ma@gmail.com>
2021-10-06 14:21:50 -0400

    fix margin/padding for icons

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -10,9 +10,12 @@ const Styles = styled.div`
 
     .navbar {
         background-color: #fff;
+        padding: 2rem 1rem;
+        
     }
     .ml-auto {
         margin-left: auto;
+       
     }
     .navbar-brand, .navbar-nav, .nav-link {
         color: #000;
@@ -21,6 +24,11 @@ const Styles = styled.div`
             color: #bbb;
         }
     }
+
+    .fa-icon {
+        font-size: 1.4rem;
+        margin-left: 3rem;
+    }
     
 `;
 
@@ -28,14 +36,14 @@ const Styles = styled.div`
 export const NavigationBar = () => {
     return (
         <Styles>
-            <Navbar expand="lg" className="navbar p-4">
+            <Navbar expand="lg" className="navbar">
                 <Navbar.Brand href="/">Logo</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto">
-                        <Nav.Item><Nav.Link className="nav-link" href="/user"> <FontAwesomeIcon icon={faUserAlt} /></Nav.Link></Nav.Item>
-                        <Nav.Item><Nav.Link className="nav-link" href="/favorites"><FontAwesomeIcon icon={faHeart} /></Nav.Link></Nav.Item>
-                        <Nav.Item><Nav.Link className="nav-link" href="/basket"><FontAwesomeIcon icon={faShoppingCart} /></Nav.Link></Nav.Item>
+                        <Nav.Item><Nav.Link className="nav-link" href="/user"> <FontAwesomeIcon className="fa-icon" icon={faUserAlt} /></Nav.Link></Nav.Item>
+                        <Nav.Item><Nav.Link className="nav-link" href="/favorites"><FontAwesomeIcon className="fa-icon" icon={faHeart} /></Nav.Link></Nav.Item>
+                        <Nav.Item><Nav.Link className="nav-link" href="/basket"><FontAwesomeIcon className="fa-icon" icon={faShoppingCart} /></Nav.Link></Nav.Item>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
