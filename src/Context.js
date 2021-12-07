1f188da9af12860e990badd4e63e42bd0f53ac72
Alena Buiko <abuiko.ma@gmail.com>
2021-12-06 21:42:15 -0500

    fetch data

--- src/Context.js
+++ src/Context.js
@@ -1,10 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Context = React.createContext()
 
 function ContextProvider(props) {
-    const [allClothes, setClothes] = useState([])
+    const [allClothes, setAllClothes] = useState([])
+    const url = "https://raw.githubusercontent.com/abuiko/react-bootstrap-assets/master/images.json"
 
+
+    useEffect(() => {
+        fetch(url)
+            .then(response => response.json())
+            .then(data => setAllClothes(data))
+    }, [])
+
+    console.log(allClothes)
     return (
         <Context.Provider value={{ allClothes }}>
             {props.children}
